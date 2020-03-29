8ed289cda0b8ea6d1f871ff3b57b1ab8d820514e
Nabson Paiva <nabsonp@gmail.com>
2020-03-28 21:10:55 -0400

    Usando o Cors.

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,9 @@ const express = require('express');
 // Importa o arquivo (por isso o ./) routes exportado pelo routes.js
 const routes = require('./routes');
 
+// Importando o Cors
+const cors = require('cors')
+
 // Instanciando a aplicação
 const app = express();
 
@@ -12,6 +15,11 @@ app.use(express.json())
 
 // Especifica que a aplicação utilize as rotas na var. routes
 app.use(routes);
+
+// Uso do cors
+// Pode receber o parâmetro origin especificando o endereço de quem
+//   pode acessar a aplicação.
+app.use(cors());
 
 // A aplicação vai ouvir a porta 3333
 app.listen(3333);
